d14a9eda0759e4931d5b6e1ae3dbfda29d313642
Maximillian Estrada <greatxam.darthart@gmail.com>
2025-06-21 23:25:48 +0800

    v0.0.0 - Initial Commit
    + Java Sprint Boot

---
+++ src/main/java/com/ophirium/stocktradingspring/StockTradingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ophirium.stocktradingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTradingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTradingSpringApplication.class, args);
+	}
+
+}
